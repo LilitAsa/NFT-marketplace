bf409ada670cf837e5eb779b32f540cbba34da0f
LilitAsa <lilitasa24@yahoo.com>
2025-09-26 18:10:50 +0300

    Initial commit: added backend and frontend

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,9 @@
+# filepath: c:\Users\HOME\Desktop\NFT\backend\accounts\serializers.py
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
